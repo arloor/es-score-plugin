67806485c8748f2fa1a0335b01f2d8a3b1f7e9d1
arloor <admin@arloor.com>
2020-04-27 10:39:20 +0800

    init

---
+++ src/main/java/org/elasticsearch/plugin/score/ExpertScriptPlugin.java
@@ -0,0 +1,154 @@
+package org.elasticsearch.plugin.score;/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.lucene.index.LeafReader;
+import org.apache.lucene.index.LeafReaderContext;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.plugins.ScriptPlugin;
+import org.elasticsearch.script.ScoreScript;
+import org.elasticsearch.script.ScoreScript.LeafFactory;
+import org.elasticsearch.script.ScriptContext;
+import org.elasticsearch.script.ScriptEngine;
+import org.elasticsearch.search.lookup.LeafDocLookup;
+import org.elasticsearch.search.lookup.SearchLookup;
+import org.elasticsearch.search.lookup.SourceLookup;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * An example script plugin that adds a {@link ScriptEngine} implementing expert scoring.
+ */
+public class ExpertScriptPlugin extends Plugin implements ScriptPlugin {
+    protected static final Logger logger = LogManager.getLogger(ExpertScriptPlugin.class);
+
+    @Override
+    public ScriptEngine getScriptEngine(Settings settings, Collection<ScriptContext<?>> contexts) {
+        return new MyExpertScriptEngine();
+    }
+
+    /**
+     * An example {@link ScriptEngine} that uses Lucene segment details to implement pure document frequency scoring.
+     */
+    // tag::expert_engine
+    private static class MyExpertScriptEngine implements ScriptEngine {
+        @Override
+        public String getType() {
+            return "expert_scripts";
+        }
+
+        @Override
+        public <T> T compile(String scriptName, String scriptSource,
+                             ScriptContext<T> context, Map<String, String> params) {
+            if (context.equals(ScoreScript.CONTEXT) == false) {
+                throw new IllegalArgumentException(getType()
+                    + " scripts cannot be used for context ["
+                    + context.name + "]");
+            }
+            // we use the script "source" as the script identifier
+            if ("horspool".equals(scriptSource)) {
+                ScoreScript.Factory factory = PureDfLeafFactory::new;
+                return context.factoryClazz.cast(factory);
+            }
+            throw new IllegalArgumentException("Unknown script name "
+                + scriptSource);
+        }
+
+        @Override
+        public void close() {
+            // optionally close resources
+        }
+
+        private static class PureDfLeafFactory implements LeafFactory {
+            private final Map<String, Object> params;
+            private final SearchLookup lookup;
+            private final List<String> fields;
+            private final String query;
+
+            private PureDfLeafFactory(
+                    Map<String, Object> params, SearchLookup lookup) {
+                if (params.containsKey("field") == false) {
+                    throw new IllegalArgumentException(
+                        "Missing parameter [field]");
+                }
+                if (params.containsKey("query") == false) {
+                    throw new IllegalArgumentException(
+                        "Missing parameter [query]");
+                }
+
+                this.params = params;
+                this.lookup = lookup;
+                fields = (List<String>) params.get("field");
+                query = params.get("query").toString();
+            }
+
+            @Override
+            public boolean needs_score() {
+                return true;  // Return true if the script needs the score
+            }
+
+            @Override
+            public ScoreScript newInstance(LeafReaderContext context)
+                throws IOException {
+                LeafReader reader = context.reader();
+                SourceLookup source = lookup.getLeafSearchLookup(context).source();
+                LeafDocLookup doc = lookup.getLeafSearchLookup(context).doc();
+                return new ScoreScript(params, lookup, context) {
+                    int currentDocid = -1;
+
+                    @Override
+                    public void setDocument(int docid) {
+                        currentDocid = docid;
+                    }
+
+                    @Override
+                    public double execute() {
+                        //获取原来的评分
+                        double rawScore = this.get_score();
+                        final double[] maxScore = {0.0};
+                        fields.forEach(fieldWeight -> {
+                            String[] split = fieldWeight.split("\\^");
+                            String field = split[0];
+                            double weight = split.length == 2 ? Double.parseDouble(split[1]) : 1;
+                            source.setSegmentAndDocument(context, currentDocid);
+                            String value = "";
+                            if (source.containsKey(field)) {
+                                value = String.valueOf(source.get(field));
+                            } else {
+                                return;
+                            }
+                            double horspool = Horspool.calHorspoolScoreWrapper(value, query);
+                            horspool = horspool * weight;
+                            if (horspool > maxScore[0]) {
+                                maxScore[0] = horspool;
+                            }
+                        });
+                        return maxScore[0];
+                    }
+                };
+            }
+        }
+    }
+    // end::expert_engine
+}
---
+++ src/main/java/org/elasticsearch/plugin/score/Horspool.java
@@ -0,0 +1,147 @@
+package org.elasticsearch.plugin.score;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 模式匹配算法
+ * 查询词作为模式串滑动匹配待搜索文档，匹配上则记录匹配上的文本和文本长度
+ * 文本去重，并按长度计算分值，分值计算逻辑为：10^n
+ * 最后将分值总和减去未匹配上文本长度的加权值：∑10^k - sum(未匹配字数) * weight
+ */
+public class Horspool {
+
+    /**
+     * @param text    待匹配文本
+     * @param pattern 模式串
+     * @return score 分值
+     */
+    public static long calHorspoolScore(String text, String pattern) {
+        text = text.toLowerCase();
+        pattern = pattern.toLowerCase();
+
+        long score = 0L;
+        int m = pattern.length();
+        int n = text.length();
+        // 游标从0开始，以待匹配文本下标作为游标
+        int cusor = 0;
+        // 游标大于endFlag表示结束，模式串已完全滑过待匹配文本
+        int endFlag = (m - 1) + (n - 1);
+        // 分值暂存集合，key：匹配上的子串；value：匹配上的子串长度
+        Map<String, Integer> groups = new HashMap<>();
+        // 记录匹配的文本字数，用于反向计算未匹配文本字数
+        int matchChars = 0;
+
+        while (cusor <= endFlag) {
+            // 模式子串匹配待匹配文本的起始下标
+            int startMatchIndex = 0;
+            // 模式子串匹配待匹配文本的截止下标，设置初始值未-1，是为了解决当模式子串第一个字符和待匹配文本匹配时，endMatchIndex = 0不代表未匹配的特殊场景
+            int endMatchIndex = -1;
+            // 连续匹配的子串长度
+            int times = 0;
+
+            // 循环子串计算对应字符是否匹配
+            for (int i = 0; i < m; i++) {
+                // 带匹配文本下标
+                int textIndex = cusor - i;
+                if (textIndex < 0 || textIndex >= n) {
+                    continue;
+                }
+
+                char T = text.charAt(textIndex);
+                char P = pattern.charAt(m - i - 1);
+
+                if (T == P) {
+                    times = times + 1;
+                    // 匹配文本字数+1
+                    matchChars = matchChars + 1;
+                    if (endMatchIndex == -1) {
+                        endMatchIndex = m - i - 1;
+                    }
+                    startMatchIndex = m - i - 1;
+                } else {
+                    if (times > 0) {
+                        String matchPhase = pattern.substring(startMatchIndex, endMatchIndex + 1);
+                        groups.put(matchPhase, matchPhase.length());
+                        endMatchIndex = -1;
+                    }
+                    times = 0;
+                }
+            }
+
+            if (endMatchIndex >= 0) {
+                String matchPhase = pattern.substring(startMatchIndex, endMatchIndex + 1);
+                groups.put(matchPhase, matchPhase.length());
+            }
+
+            cusor++;
+        }
+
+        for (Map.Entry<String, Integer> entry : groups.entrySet()) {
+            score = score + (long) Math.pow(10, entry.getValue());
+        }
+
+        score = Math.max(score / 2, score - (n - matchChars) * 5);
+        return score;
+    }
+
+
+    public static long cal(String text, String pattern) {
+        long score = 0L;
+        int m = pattern.length();
+        int n = text.length();
+        int endFlag = (m - 1) + (n - 1);
+
+        int times = 0;
+        int cusor = 0;
+        while (cusor <= endFlag) {
+            for (int i = 0; i < m; i++) {
+                int textIndex = cusor - i;
+                if (textIndex < 0 || textIndex >= n) {
+                    continue;
+                }
+
+                char T = text.charAt(textIndex);
+                char P = pattern.charAt(m - i - 1);
+
+                if (T == P) {
+                    times = times + 1;
+                    score = score + (long) Math.pow(10, times);
+                } else {
+                    times = 0;
+                }
+            }
+            cusor++;
+        }
+
+        return score;
+    }
+
+    private static boolean empty(String s){
+        return s == null ||s.trim().length() == 0;
+    }
+
+    public static long calHorspoolScoreWrapper(String text, String pattern) {
+        if (empty(text) || empty(pattern)) {
+            return 0L;
+        }
+        // 超过30截断只取前30位匹配
+        if (text.length() > 30) {
+            text = text.substring(0, 30);
+        }
+
+        return calHorspoolScore(text, pattern);
+    }
+
+    public static void main(String[] args) {
+        String text = "测试新建";
+
+        String pattern = "测试";
+        print(text, pattern);
+    }
+
+    public static void print(String text, String pattern) {
+        long score = Horspool.calHorspoolScoreWrapper(text, pattern);
+        System.out.println(pattern + ": " + score);
+    }
+}
